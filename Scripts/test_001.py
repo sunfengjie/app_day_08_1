f13bb6fb820c769c08e73e45a8a68a5813e1b00f
sunfengjie <sunfengjie1225@163.com>
2018-07-06 20:13:30 +0800

    更新的代码

---
+++ Scripts/test_001.py
@@ -0,0 +1,18 @@
+import allure
+import pytest
+
+class Test_allure:
+    def setup(self):
+        pass
+    def teardown(self):
+        pass
+    def test_01(self):
+        assert 0
+    def test_02(self):
+        assert 1
+
+
+
+
+
+
