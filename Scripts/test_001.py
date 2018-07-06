f7d8e888e8849ca0a01669f872b956f6b0713e4e
sunfengjie <sunfengjie1225@163.com>
2018-07-06 21:42:13 +0800

    再次更新

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -7,11 +7,12 @@ class Test_allure:
     def teardown(self):
         pass
 
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @pytest.mark.parametrize("a",[1,2,3])
     @allure.step("我是测试步骤001")
     def test_01(self,a):
         assert a != 2
-
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     def test_02(self):
 
         allure.attach("描述","我是测试步骤02的描述-----")
